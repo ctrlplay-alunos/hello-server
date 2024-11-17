dbbd9e1ced2ac948ae0d413b7dc408de980a0802
jacqueline <jacquelinecastro.dev@gmail.com>
2024-11-17 13:55:20 -0300

    refactor: 💡 remove port const

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const app = express();             
-const port = 8000;             
+const app = express();                          
 
 app.get('/', (_req, res) => {
   res.send('Hello server!'); 
 });
 
-app.listen(port, () => {
-  console.log(`O servidor está rodando em http://localhost:${port}`);
+app.listen(8000, () => {
+  console.log("O servidor está rodando em http://localhost:8000");
 });
